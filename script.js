9175255c1e0cf609b6b8b40f110608b281c3ce56
MuhammadOsmanGani <osmangani.cse12@gmail.com>
2025-08-29 23:06:50 +0600

    emeragency service, police , and ambulance

--- script.js
+++ script.js
@@ -8,3 +8,36 @@ document.querySelectorAll('.heartPress').forEach(button => {
         document.getElementById("totalHeart").innerText = totalCount;
     });
 });
+// Copy function
+document.querySelectorAll('.copyBtn').forEach(button => {
+    button.addEventListener("click", () => {
+
+        button.parentElement.querySelector('.copyBtn').innerHTML = "Copied..";
+
+        copyCount++;
+        document.querySelector('.copyCount span').innerText = copyCount;
+
+        const Number = parseInt(button.parentElement.parentElement.querySelector('.copyNum').innerText);
+        navigator.clipboard.writeText(Number);
+
+
+    });
+});
+// Call History
+document.querySelectorAll('.callbtn').forEach(button => {
+    button.addEventListener("click", () => {
+        const callText = button.parentElement.parentElement.querySelector('.HistoryTitle').innerText;
+        const Number = button.parentElement.parentElement.querySelector('.copyNum').innerText;
+        const div = document.createElement("div");
+        div.innerHTML = ` <div class="flex flex-row justify-between items-center mt-4 px-4 py-4 rounded-lg">
+                       <div class="flex flex-col">
+                           <p>${callText}</p>
+                           <p>${Number}</p>
+                       </div>
+                       <div><p>${new Date().toLocaleTimeString()}</p></div>
+                   </div>`;
+
+
+        document.querySelector('.callHistory').appendChild(div);
+    });
+});
